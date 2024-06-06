0709bf73567cd89eb8a3b3be704bb3c0bf20eb61
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-06-06 17:08:28 -0500

    Insert new register for streamer and video

--- CleanArchitecture.App/Program.cs
+++ CleanArchitecture.App/Program.cs
@@ -1,19 +1,54 @@
-﻿using CleanArchitecture.Data.Context;
+﻿using System.Net;
+using System.Text.Json;
+using CleanArchitecture.App;
+using CleanArchitecture.Data.Context;
 using CleanArchitecture.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 StreamerContext context = new();
 //await AddNewRecords();
 //await QueryFilter();
-await TrackingAndNotTracking();
+//await TrackingAndNotTracking();
+//await TestWizeLine();
+await AddNewStreamerWithVideo();
 Console.ReadLine();
 return;
+
+async Task TestWizeLine()
+{
+    var client = new HttpClient();
+    using var response = await client.GetAsync("https://api.sampleapis.com/cartoons/cartoons2D");
+    var jsonResponse = await response.Content.ReadAsStringAsync();
+    //Console.WriteLine($"JSON RESPONSE {jsonResponse}");
+    var cartoonSerialized = JsonSerializer.Deserialize<List<Cartoon>>(jsonResponse);
+    cartoonSerialized?.ForEach(x =>
+        Console.WriteLine(
+            $"Cartoon: Title: {x.Title}, Year: {x.Year}, Creator: {x.Creator}, Rating: {x.Rating}, Genre: {x.Genre}, Runtime: {x.RuntimeInMinutes}, Episodes: {x.Episodes}, Image: {x.Image}, ID: {x.Id}"));
+}
+
+
+async Task AddNewStreamerWithVideo()
+{
+    var streamer = new Streamer
+    {
+        Name = "west"
+    };
+    
+    var video = new Video
+    {
+        Name = $"Video from {streamer.Name}",
+        Streamer = streamer
+    };
+    
+    await context.Streamers!.AddAsync(streamer);
+    await context.SaveChangesAsync();
+}
 
 async Task TrackingAndNotTracking()
 {
     var streamerTracking = await context.Streamers!.FirstOrDefaultAsync(x => x.Id == 1);
     var streamerNotTracking = await context.Streamers!.AsNoTracking().FirstOrDefaultAsync(x => x.Id == 2);
-    
+
     streamerTracking.Name = "Streamer 1 tracking";
     streamerNotTracking.Name = "Streamer 2 no tracking";
     await context.SaveChangesAsync();
@@ -23,8 +58,9 @@ async Task QueryLinq()
 {
     Console.WriteLine("Enter a streamer name:");
     var streamerName = Console.ReadLine();
-    
-    var streamers = await (from i in context.Streamers where EF.Functions.Like(i.Name, $"%{streamerName}%") select i).ToListAsync();
+
+    var streamers = await (from i in context.Streamers where EF.Functions.Like(i.Name, $"%{streamerName}%") select i)
+        .ToListAsync();
     streamers.ForEach(x => Console.WriteLine($"streamer: {x.Name}, with ID {x.Id}"));
 }
 
@@ -41,10 +77,11 @@ async Task QueryFilter()
     Console.WriteLine("Enter a streaming company:");
     var streamingCompany = Console.ReadLine() ?? "";
     var streamers = await context.Streamers!.Where(s => s.Name!.Equals(streamingCompany)).ToListAsync();
-    streamers.ForEach(x => Console.WriteLine($"Streamer Query: {x.Id} - {x.Name}" ));
-    
-    var streamingPartial = await context.Streamers!.Where(s => EF.Functions.Like(s.Name, $"%{streamingCompany}%")).ToListAsync();
-    streamingPartial.ForEach(x => Console.WriteLine($"Streamer Query: {x.Id} - {x.Name}" ));
+    streamers.ForEach(x => Console.WriteLine($"Streamer Query: {x.Id} - {x.Name}"));
+
+    var streamingPartial = await context.Streamers!.Where(s => EF.Functions.Like(s.Name, $"%{streamingCompany}%"))
+        .ToListAsync();
+    streamingPartial.ForEach(x => Console.WriteLine($"Streamer Query: {x.Id} - {x.Name}"));
 }
 
 void QueryStreaming()
@@ -93,6 +130,6 @@ async Task AddNewRecords()
 
     await context.AddRangeAsync(movies);
     await context.SaveChangesAsync();
-    
+
     QueryStreaming();
 }
